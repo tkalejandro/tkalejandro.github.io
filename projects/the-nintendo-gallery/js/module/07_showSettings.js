8d7bf5246f0f0e1a15c77b6af965835d3affa86f
Alejandro <tk.jalejandro@gmail.com>
2021-08-25 09:33:53 +0200

    the  nintenfo gallery project updated

--- projects/the-nintendo-gallery/js/module/07_showSettings.js
+++ projects/the-nintendo-gallery/js/module/07_showSettings.js
@@ -1,4 +1,4 @@
-import { galleryContainer, innerSettingsContainer, settingsBtn, settingTitle } from "./00_variables.js"
+import { columnBtnContainer, columnSelectContainer, franchiseBtn, franchiseCheckbox, galleryContainer, innerSettingsContainer, numberGallery, numberGalleryBtn, settingsBtn, settingTitle } from "./00_variables.js"
 
 
 //toggle help to add and remove the same class, no need to create extra logic
@@ -17,5 +17,35 @@ export const showSettings = (event) => {
 
    //Now we when h2 appears we need to make sure is invisible, we want to keep it just for semantic purposes
    settingTitle.classList.toggle("v-hidden")
+
+   //Now we default all options to make sure everytime we close or open it goes to their initial state!.
+   //This is same to the Index HTML. Nothing special.
+   //Images Btn
+   numberGalleryBtn.className = "numberGalleryBtn far fa-images"
+   numberGallery.className = "d-none"
+   //Columns Btn
+   columnBtnContainer.className = "columnBtnContainer"
+   columnSelectContainer.className = "columnSelectContainer d-none"
+   //Franchise Btn
+   franchiseCheckbox.className = "franchiseCheckbox d-none"
+   franchiseBtn.className = "fas fa-gamepad franchiseBtn"   
+}
+
+export const showColumnSettings = (event) => {
+   //Avoid propagation!
+   event.stopPropagation()
+   
+   columnBtnContainer.classList.toggle("d-none")
+   columnSelectContainer.classList.toggle("d-none")
+}
+
+export const showNumberGallerySettings = (event) => {
+   //Avoid propagation
+   event.stopPropagation()
+
+   //Now im invisible!
+   numberGalleryBtn.classList.toggle("d-none")
+   //Now im visible!
+   numberGallery.classList.toggle("d-none")
 }
 
